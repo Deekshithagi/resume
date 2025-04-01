efbc2d227c9a470996b14dec093413b0833fc823
Deekshithagi <kdeekshitha647@gmail.com>
2025-04-01 16:59:08 +0530

    added new folder

---
+++ test.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+def test_homepage():
+    response = client.get("/")
+
+    assert response.status_code==200
+    assert "<title>Resume</title>" in response.text
